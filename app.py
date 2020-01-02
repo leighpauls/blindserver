129742d06e4e8b1dc4d8574095c9a173aa6cdd92
Ubuntu <ubuntu@ip-172-31-35-24.us-east-2.compute.internal>
2020-01-02 02:56:42 +0000

    get stupid auth working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, escape, request
+import json
 
 app = Flask(__name__)
 
@@ -11,5 +12,29 @@ def hello():
 def healthz():
     return 'OK'
 
+@app.route('/fulfill')
+def fulfill():
+    return 'OK'
+
+@app.route('/auth')
+def auth():
+    redirect_uri = request.args['redirect_uri']
+    state = request.args['state']
+    code = '1234poop'
+    return f'<a href="{redirect_uri}?code={code}&state={state}">OK</a>'
+
+@app.route('/token', methods=['POST'])
+def token():
+    result = {
+        'token_type': 'Bearer',
+        'access_token': 'someAccessToken',
+        'expires_in': 43200
+    }
+
+    if request.form['grant_type'] == 'authorization_code':
+        result['refresh_token'] = 'someRefreshToken'
+    return json.dumps(result)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
