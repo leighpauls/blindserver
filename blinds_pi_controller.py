3edf3755c670ff2262e8fcd92693e8cdcd3d8bfa
leighpauls <leigh.pauls@gmail.com>
2020-03-07 18:35:18 -0800

    OOP-ify the single blind controller

--- blinds_pi_controller.py
+++ blinds_pi_controller.py
@@ -5,8 +5,45 @@ import time
 
 import protocol
 
-command_position: Optional[str] = None
-command_expires_time: Optional[float] = None
+
+class BlindIO:
+    up_sensor: gpiozero.Button
+    down_sensor: gpiozero.Button
+    up_motor: gpiozero.LED
+    down_motor: gpiozero.LED
+
+    def __init__(self, up_sensor: int, down_sensor: int, up_motor: int, down_motor: int):
+        self.up_sensor = gpiozero.Button(up_sensor)
+        self.down_sensor = gpiozero.Button(down_sensor)
+        self.up_motor = gpiozero.LED(up_motor)
+        self.down_motor = gpiozero.LED(down_motor)
+
+
+class BlindController:
+    io: BlindIO
+    command_position: Optional[bytes] = None
+    command_expires_time: Optional[float] = None
+
+    def __init__(self, io: BlindIO):
+        self.io = io
+        self.command_position = None
+        self.command_expires_time = None
+
+    def control_step(self, cur_time: float):
+        # Check if the command is completed or expired
+        if (self.command_expires_time and cur_time > self.command_expires_time) \
+           or (self.command_position == protocol.OPEN_CMD and self.io.up_sensor.is_pressed) \
+           or (self.command_position == protocol.CLOSE_CMD and self.io.down_sensor.is_pressed):
+            self.command_expires_time = None
+            self.command_position = None
+
+        self.io.up_motor.value = self.command_position == protocol.OPEN_CMD
+        self.io.down_motor.value = self.command_position == protocol.CLOSE_CMD
+
+
+blinds = [
+    BlindController(BlindIO(2, 3, 17, 27))
+]
 
 COMMAND_EXPIRES_SECONDS = 5.0
 
@@ -17,54 +54,32 @@ async def on_connected(reader, writer):
         print("unknown command received")
         return
     print(f'recieved {data} network command')
-    set_command(data)
+    set_command(data, time.monotonic())
 
-def set_command(new_position):
-    global command_position
-    global command_expires_time
-    command_position = new_position
-    command_expires_time = time.monotonic() + COMMAND_EXPIRES_SECONDS
+def set_command(new_position: bytes, cur_time: float):
+    global blinds
+    for b in blinds:
+        b.command_position = new_position
+        b.command_expires_time = cur_time + COMMAND_EXPIRES_SECONDS
 
-async def control_loop():
+async def control_loop() -> None:
     up_command_button = gpiozero.Button(14)
     down_command_button = gpiozero.Button(15)
 
-    up_sensor = gpiozero.Button(2)
-    down_sensor = gpiozero.Button(3)
-
-    up_motor = gpiozero.LED(17)
-    down_motor = gpiozero.LED(27)
-
-    global command_position
-    global command_expires_time
+    global blinds
 
     while True:
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.05)
+
+        cur_time = time.monotonic()
 
         if up_command_button.is_pressed and not down_command_button.is_pressed:
-            set_command(protocol.OPEN_CMD)
+            set_command(protocol.OPEN_CMD, cur_time)
         elif down_command_button.is_pressed and not up_command_button.is_pressed:
-            set_command(protocol.CLOSE_CMD)
+            set_command(protocol.CLOSE_CMD, cur_time)
 
-        # Check if the command is completed or expired
-        if (command_expires_time and time.monotonic() > command_expires_time) \
-           or (command_position == protocol.OPEN_CMD and up_sensor.is_pressed) \
-           or (command_position == protocol.CLOSE_CMD and down_sensor.is_pressed):
-            command_expires_time = None
-            command_position = None
-
-        output_up = command_position == protocol.OPEN_CMD
-        output_down = command_position == protocol.CLOSE_CMD
-
-        up_motor.value = output_up
-        down_motor.value = output_down
-
-        print(f'up command button: {up_command_button.is_pressed},'
-              f' down command button: {down_command_button.is_pressed},'
-              f' desired_pos: {command_position},'
-              f' up_sensor: {up_sensor.is_pressed},'
-              f' down_sensor: {down_sensor.is_pressed},'
-              f' output: u:{output_up} d:{output_down}')
+        for b in blinds:
+            b.control_step(cur_time)
 
 def main() -> None:
     loop = asyncio.get_event_loop()
